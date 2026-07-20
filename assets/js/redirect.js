643ae605533de39f2375d41ffdb769cc74fdeedb
William Burkert <willburkert96@gmail.com>
2026-07-20 12:18:17 +0100

    added security around redirect

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,12 +1,24 @@
 /*jslint long*/
+/*global window, document, setInterval, clearInterval*/
+
 let seconds = 10;
 let countdownEl = document.getElementById("countdown");
+
+let pathParts = window.location.pathname
+    .split("/")
+    .filter(Boolean);
+    
+let homePath = window.location.hostname.endsWith("github.io")
+    ? "/" + pathParts[0] + "/"
+    : "/";
+
 
 let timer = setInterval(function () {
     seconds -= 1;
     countdownEl.textContent = seconds;
+
     if (seconds <= 0) {
         clearInterval(timer);
-        window.location.href = "https://hybrid965.github.io/loyal-paws-art/index.html";
+        window.location.href = homePath;
     }
 }, 1000);
